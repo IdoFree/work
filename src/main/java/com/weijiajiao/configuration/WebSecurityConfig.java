3dcdb48e1ba92778f51650f3366e0497162ff129
tough <363652003@qq.com>
2017-06-20 16:34:17 +0800

    推到外网测试服务器

--- src/main/java/com/weijiajiao/configuration/WebSecurityConfig.java
+++ src/main/java/com/weijiajiao/configuration/WebSecurityConfig.java
@@ -1,40 +1,41 @@
 package com.weijiajiao.configuration;
 
-import com.weijiajiao.service.AccountService;
+//import com.weijiajiao.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private AccountService accountService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(accountService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/area/**", "/article/**","/teacher/search","/teacher/{teacherId}").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic().and()
-                .csrf().disable();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**", "/**/favicon.ico");
-    }
-
-}
+//@EnableWebSecurity
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private AccountService accountService;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.userDetailsService(accountService).passwordEncoder(new BCryptPasswordEncoder());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/area/**", "/article/**","/teacher/search","/teacher/{teacherId}").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic().and()
+//                .csrf().disable();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**", "/**/favicon.ico");
+//    }
+//
+//}
